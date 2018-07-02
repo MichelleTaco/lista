bc2ca8b32fa0957f05101f620236f317e0caffe3
MichelleTaco <36052070+MichelleTaco@users.noreply.github.com>
2018-07-02 05:15:40 -0500

    first commit

---
+++ lista.cs
@@ -0,0 +1,18 @@
+class ordenadorNumeros
+{
+    Lista<int>Enteros(1,2,3,5,6,8,0,3,6);
+    public Lista<int>ordenar(Lista <int> listaDesordenada)
+    {
+        while(listaDesordenada.count()>0);
+        var listaDesordenada= new Lista<int>();
+        var imenor=0
+        for (i=0; i<listaDesordenada.count(); i++)
+        {
+            listaDesordenada[i]<listaDesordenada[imenor]
+            imenor=i;
+        }
+        listaOrdenada.add(listaDesordenada[imenor]);
+        listaDesordenada.remove(imenor);
+    }
+    retun listaOrdenada;
+}
